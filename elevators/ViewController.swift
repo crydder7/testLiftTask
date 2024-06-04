6ba32bc26a62623527aceafda2077b94f47e4cdb
crydder7 <getright1337ez@gmail.com>
2024-06-04 11:12:04 +0300

    Initial Commit

---
+++ elevators/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  elevators
+//
+//  Created by lonely. on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
